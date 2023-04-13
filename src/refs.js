74c3e4d91956517b8b48f6cda3da96c7f84ac734
Hordiyenko Tetyana <hordit@me.com>
2023-04-13 18:52:46 +0200

    fetchApi

---
+++ src/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    inputForm: document.querySelector('.input-text'),
+    searchBtn: document.querySelector('.searchBtn'),
+    galleryItems: document.querySelector('.gallery'),
+}
---
+++ src/settings.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = 'ad57a1cb54651b0ec3ddf406d1d39fd8';
