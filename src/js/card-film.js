b499e33e34b61bc9b27cb7a2182e83ff395a9761
Olha <103729727+Demouser888@users.noreply.github.com>
2023-04-14 23:48:11 +0300

    add changes

--- src/js/card-film.js
+++ src/js/card-film.js
@@ -5,16 +5,18 @@ export function createMarkup(data) {
   return data.results.map(({ id, poster_path, title, backdrop_path, genre_ids, release_date }) => {
     const genres = genre_ids.map(id => MovieApi.getGenreNameById(id));
     return `
-      <li data-id="${id}">
-        <img class="poster" src="${poster_path}" alt="${title}"
+      <li class="gallery__item" data-id="${id}">
+        <img class="poster gallery__item-image" src="${poster_path}" alt="${title}"
           data-source="https://image.tmdb.org/t/p/w500${backdrop_path}" />
-        <h3 class="film-name ">
+          <div class="gallery__description">
+          <p class=" gallery__description-title film-name ">
           ${title}
-        </h3>
-        <p class="film-genres">
+        </p>
+        <p class="gallery__description-genres film-genres">
           ${genres.map((genre) => `<span class="film-genre">${genre}</span>`).join('')}
           | ${release_date}
         </p>
+        </div>
       </li>
     `;
   }).join('');
