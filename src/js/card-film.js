8b722d61c6d695fd88b150a21b1a21b3f1892a89
Olha <103729727+Demouser888@users.noreply.github.com>
2023-04-15 14:34:25 +0300

    change gallery

--- src/js/card-film.js
+++ src/js/card-film.js
@@ -1,32 +1,32 @@
-import MovieApi from './apiService';
-
-export function createMarkup(data) {
-  return data.results
-    .map(
-      ({ id, poster_path, title, backdrop_path, genre_ids, release_date }) => {
-        const genres = genre_ids.map(id => MovieApi.getGenreNameById(id));
+import { refs } from './refs';
+import genresData from '../genres.json';
+export const genres = genresData.genres;
+// Рендер карточек с популярными фильмами
+export async function renderMovies(data) {
+  if (!data) {
+    return;
+  }
+  refs.gallery.innerHTML = data
+    .map(movie => {
+      const imageSrc = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png';
+        const genres = genersForFilmCard(movie.genre_ids);
+        const year = movie.release_date
+          ? movie.release_date.split('-')[0]
+          : 'n/a';
         return `
-      <li class="gallery__item" data-id="${id}">
-        <img class="poster gallery__item-image" src="${poster_path}" alt="${title}"
-          data-source="https://image.tmdb.org/t/p/w500${backdrop_path}" />
-          <div class="gallery__description">
-          <p class=" gallery__description-title film-name ">
-          ${title}
-        </p>
-        <p class="gallery__description-genres film-genres">
-          ${genres
-            .map(genre => `<span class="film-genre">${genre}</span>`)
-            .join('')}
-          | ${release_date}
-        </p>
-        </div>
+    <li class="gallery__item" data-id="${movie.id}">
+      <img class="gallery__item-image" alt="${movie.title}" src="${imageSrc}">
+      <div class="gallery__item-description">
+      <p class="gallery__item-description-title">${movie.title}</p>
+      <p class="gallery__item-description-genres">${genres} | ${year}</p>
+      </div>
       </li>
-    `;
-      }
-    )
+`;
+    })
     .join('');
 }
-
 // Генерируем жанры по id-шнику
 function genersForFilmCard(arrayOfGenersID) {
   const arrayOfGenres = arrayOfGenersID.map(id => {
@@ -36,6 +36,5 @@ function genersForFilmCard(arrayOfGenersID) {
     arrayOfGenres.length > 2
       ? arrayOfGenres.splice(0, 2).concat('Other').join(', ')
       : arrayOfGenres.join(', ');
-
   return result;
 }
