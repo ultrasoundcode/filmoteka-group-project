c589f6ef9b63ba29c8bc389f38ba07e337c3eeed
Evgeniy <114279264+dem-ov@users.noreply.github.com>
2023-04-15 10:53:06 +0300

    Create rendering-cards.js

---
+++ src/js/storage/rendering-cards.js
@@ -0,0 +1,33 @@
+const searchInput = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
+
+function searchMovies(query) {
+  const filteredMovies = savedMovies.filter(movie => {
+    const title = movie.title.toLowerCase();
+    const queryLower = query.toLowerCase();
+    return title.includes(queryLower);
+  });
+
+  renderSavedMovies(filteredMovies);
+}
+
+searchButton.addEventListener('click', () => {
+  const query = searchInput.value.trim();
+  if (query === '') {
+    renderSavedMovies(savedMovies);
+    return;
+  }
+  searchMovies(query);
+});
+
+//
+
+searchInput.addEventListener('keydown', event => {
+  if (event.keyCode === 13) {
+    const query = searchInput.value.trim();
+  }
+});
+//
+//
+//
+//
