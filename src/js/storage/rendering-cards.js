5181344fa951255fabb63a1a4eb9d1b92863a288
Evgeniy <114279264+dem-ov@users.noreply.github.com>
2023-04-15 10:49:20 +0300

    Update rendering-cards.js

--- src/js/storage/rendering-cards.js
+++ src/js/storage/rendering-cards.js
@@ -20,12 +20,13 @@ searchButton.addEventListener('click', () => {
   searchMovies(query);
 });
 
-// 
+//
 
 searchInput.addEventListener('keydown', event => {
   if (event.keyCode === 13) {
     const query = searchInput.value.trim();
   }
 });
-// 
-// 
+//
+//
+//
