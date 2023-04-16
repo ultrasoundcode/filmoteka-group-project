4e746b581f8d1d6c5ebc2c95296d966dd45e5ef5
Evgeniy <114279264+dem-ov@users.noreply.github.com>
2023-04-16 12:42:55 +0300

    Update rendering-cards.js

--- src/js/storage/rendering-cards.js
+++ src/js/storage/rendering-cards.js
@@ -1,10 +1,10 @@
-import galleryMovies from '../partials/templates/film.hbs';
+import renderMovies from '../card-film';
 import { getRefs } from '../refs';
 
 export const renderGallery = function (objects) {
   //добавляет в конец по шаблону карточки фильмов
   const refs = getRefs();
-  refs.galleryItems.insertAdjacentHTML('beforeend', galleryMovies(objects));
+  refs.galleryItems.insertAdjacentHTML('beforeend', renderMovies(objects));
 };
 
 export const renderAllGallery = function (object) {
