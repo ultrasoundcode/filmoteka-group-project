8768ffafcf6a38a4c6576afd93d5745933da1597
Hordiyenko Tetyana <hordit@me.com>
2023-08-24 11:43:23 +0200

    update pagination andheader css

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -17,21 +17,7 @@ export default class MovieApi {
     hideLoader();
     return response.data.results;
   }
-  async getData() {
-    showLoader();
-    const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
-    const response = await axios.get(url);
-    hideLoader();
-    return response.data;
-  }
-  async getMoviesForPagination(page) {
-    showLoader();
-    const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
-    const response = await axios.get(url);
-    hideLoader();
-    return response.data.results;
-  }
-
+ 
   async getTrendingMoviesForPagination(page) {
     showLoader();
     const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
@@ -40,7 +26,15 @@ export default class MovieApi {
     return response.data.results;
   }
 
-  async searchMovies(query) {
+  async getMoviesCount() {
+    showLoader();
+    const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
+    const response = await axios.get(url);
+    hideLoader();
+    return response.data.total_results;
+  }
+
+  async getSearchMovies(query) {
     showLoader();
     const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`;
     const response = await axios.get(url);
@@ -54,6 +48,14 @@ export default class MovieApi {
     const response = await axios.get(url);
     hideLoader();
     return response.data.results;
+  }
+
+  async getSearchMoviesCount(query) {
+    showLoader();
+    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`;
+    const response = await axios.get(url);
+    hideLoader();
+    return response.data.total_results;
   }
 
   async getMovieDetails(movieId) {
@@ -71,19 +73,97 @@ export default class MovieApi {
     hideLoader();
     return response.data.genres;
   }
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
 }
+
+
+
+
+// import axios from 'axios';
+// import { BASE_URL, API_KEY } from './settings';
+// import { showLoader, hideLoader } from './loader';
+
+// export default class MovieApi {
+//   constructor() {
+//     this.page = 1;
+//     this.genreId = '';
+//     this.genre = '';
+//     this.searchQuery = '';
+//   }
+
+//   async getTrendingMovies() {
+//     showLoader();
+//     const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.results;
+//   }
+//   async getData() {
+//     showLoader();
+//     const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data;
+//   }
+//   async getMoviesForPagination(page) {
+//     showLoader();
+//     const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.results;
+//   }
+
+//   async getTrendingMoviesForPagination(page) {
+//     showLoader();
+//     const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.results;
+//   }
+
+//   async searchMovies(query) {
+//     showLoader();
+//     const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.results;
+//   }
+
+//   async getSearchMoviesForPagination(query, page) {
+//     showLoader();
+//     const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.results;
+//   }
+
+//   async getMovieDetails(movieId) {
+//     showLoader();
+//     const url = `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data;
+//   }
+
+//   async getGenres() {
+//     showLoader();
+//     const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
+//     const response = await axios.get(url);
+//     hideLoader();
+//     return response.data.genres;
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+// }
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -16,8 +16,6 @@ import {
   makeMyLibraryActive,
 } from './nav-bar/nav-btns-functions';
 
-const movieApi = new MovieApi();
-
 const pagination = new Pagination(refs.pagContainer, {
   totalItems: 0,
   itemsPerPage: 20,
@@ -44,54 +42,169 @@ const pagination = new Pagination(refs.pagContainer, {
       '</a>',
   },
 });
-let totalMovies = 400;
-pagination.reset(totalMovies);
-// Home page pagination
-refs.homeBtn.addEventListener('click', activateHomePagination);
 
-function activateHomePagination() {
-  pagination.reset(400);
-  pagination.on('beforeMove', async event => {
+const movieApi = new MovieApi();
+
+movieApi.getMoviesCount().then(count => {
+pagination.reset(count);
+});
+
+//   Home page and movie search pagination
+refs.searchInput.addEventListener('input', async () => {
+  const query = refs.searchInput.value.trim();
+  const totalItems = query
+    ? await movieApi.getSearchMoviesCount(query)
+    : await movieApi.getMoviesCount(query);
+  pagination.reset(totalItems);
+});
+
+refs.homeBtn.addEventListener('click', async () => {
+  makeHomeActive();
+  pagination.reset(await movieApi.getMoviesCount());
+});
+
+pagination.on('beforeMove', async event => {
     const query = refs.searchInput.value.trim();
     if (query) {
-      makeHomeActive();
       await movieApi.getSearchMoviesForPagination(query, event.page).then(r => {
-        renderMovies(r);
+      renderMovies(r);
       });
     } else {
-      makeHomeActive();
-      await movieApi.getMoviesForPagination(event.page).then(r => {
-        renderMovies(r);
+      await movieApi.getTrendingMoviesForPagination(event.page).then(r => {
+      renderMovies(r);
       });
     }
-  });
-}
+});
 
-// // Library pagination
+//   Library pagination
 refs.libraryBtn.addEventListener('click', activateLibraryPagination);
+refs.queueBtn.addEventListener('click', activateQeueuePagination);
+refs.watchedBtn.addEventListener('click', activateWatchedPagination);
+
 function activateLibraryPagination() {
   refs.watchedBtn.classList.remove('active');
   refs.queueBtn.classList.remove('active');
   pagination.reset(getAllMoviesFromLocalStorage().length);
+
   pagination.on('beforeMove', async event => {
     makeMyLibraryActive();
-
     refs.homeBtn.classList.remove('active');
-
     renderMoviesByPageNumber(event.page);
   });
 }
-refs.queueBtn.addEventListener('click', activateQeueuePagination);
+
 function activateQeueuePagination() {
   pagination.reset(getFromLocalStorage('queue').length);
   pagination.on('beforeMove', async event => {
     renderQueueMoviesByPageNumber(event.page);
   });
 }
-refs.watchedBtn.addEventListener('click', activateWatchedPagination);
+
 function activateWatchedPagination() {
   pagination.reset(getFromLocalStorage('watched').length);
   pagination.on('beforeMove', async event => {
     renderWatchedMoviesByPageNumber(event.page);
   });
 }
+
+
+
+
+
+// import Pagination from 'tui-pagination';
+// import { refs } from './refs';
+// import MovieApi from './apiService';
+// import { renderMovies } from './card-film-render';
+// import {
+//   renderMoviesByPageNumber,
+//   renderQueueMoviesByPageNumber,
+//   renderWatchedMoviesByPageNumber,
+// } from './library/initial-library-render';
+// import {
+//   getAllMoviesFromLocalStorage,
+//   getFromLocalStorage,
+// } from './storage/ls-data-services';
+// import {
+//   makeHomeActive,
+//   makeMyLibraryActive,
+// } from './nav-bar/nav-btns-functions';
+
+// const movieApi = new MovieApi();
+
+// const pagination = new Pagination(refs.pagContainer, {
+//   totalItems: 0,
+//   itemsPerPage: 20,
+//   visiblePages: 5,
+//   page: 1,
+//   centerAlign: true,
+//   firstItemClassName: 'tui-first-child',
+//   lastItemClassName: 'tui-last-child',
+//   template: {
+//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//     currentPage:
+//       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//       '<span class="tui-ico-ellip">...</span>' +
+//       '</a>',
+//   },
+// });
+// let totalMovies = 400;
+// pagination.reset(totalMovies);
+// // Home page pagination
+// refs.homeBtn.addEventListener('click', activateHomePagination);
+
+// function activateHomePagination() {
+//   pagination.reset(400);
+//   pagination.on('beforeMove', async event => {
+//     const query = refs.searchInput.value.trim();
+//     if (query) {
+//       makeHomeActive();
+//       await movieApi.getSearchMoviesForPagination(query, event.page).then(r => {
+//         renderMovies(r);
+//       });
+//     } else {
+//       makeHomeActive();
+//       await movieApi.getMoviesForPagination(event.page).then(r => {
+//         renderMovies(r);
+//       });
+//     }
+//   });
+// }
+
+// // // Library pagination
+// refs.libraryBtn.addEventListener('click', activateLibraryPagination);
+// function activateLibraryPagination() {
+//   refs.watchedBtn.classList.remove('active');
+//   refs.queueBtn.classList.remove('active');
+//   pagination.reset(getAllMoviesFromLocalStorage().length);
+//   pagination.on('beforeMove', async event => {
+//     makeMyLibraryActive();
+
+//     refs.homeBtn.classList.remove('active');
+
+//     renderMoviesByPageNumber(event.page);
+//   });
+// }
+// refs.queueBtn.addEventListener('click', activateQeueuePagination);
+// function activateQeueuePagination() {
+//   pagination.reset(getFromLocalStorage('queue').length);
+//   pagination.on('beforeMove', async event => {
+//     renderQueueMoviesByPageNumber(event.page);
+//   });
+// }
+// refs.watchedBtn.addEventListener('click', activateWatchedPagination);
+// function activateWatchedPagination() {
+//   pagination.reset(getFromLocalStorage('watched').length);
+//   pagination.on('beforeMove', async event => {
+//     renderWatchedMoviesByPageNumber(event.page);
+//   });
+// }
